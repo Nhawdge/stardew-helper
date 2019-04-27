3e72dc8f243a5ca51125673cf22219115f524545
John Pavek <JohnQPavek@gmail.com>
2019-04-27 13:03:15 -0500

    More changes. Adds events

---
+++ data/data.js
@@ -0,0 +1,234 @@
+// Don't judge. I don't want to deal with ajax & github.
+
+var data = {
+    "birthdays": [
+        {
+            "name": "Kent",
+            "date": "4",
+            "season": "spring"
+        },
+        {
+            "name": "Lewis",
+            "date": "7",
+            "season": "spring"
+        },
+        {
+            "name": "Vincent",
+            "date": "3",
+            "season": "spring"
+        },
+        {
+            "name": "Haley",
+            "date": "14",
+            "season": "spring"
+        },
+        {
+            "name": "Pam",
+            "date": "18",
+            "season": "spring"
+        },
+        {
+            "name": "Shane",
+            "date": "20",
+            "season": "spring"
+        },
+        {
+            "name": "Pierre",
+            "date": "26",
+            "season": "spring"
+        },
+        {
+            "name": "Emily",
+            "date": "27",
+            "season": "spring"
+        },
+        {
+            "name": "Jas",
+            "date": "04",
+            "season": "summer"
+        },
+        {
+            "name": "Gus",
+            "date": "08",
+            "season": "summer"
+        },
+        {
+            "name": "Maru",
+            "date": "10",
+            "season": "summer"
+        },
+        {
+            "name": "Alex",
+            "date": "13",
+            "season": "summer"
+        },
+        {
+            "name": "Sam",
+            "date": "17",
+            "season": "summer"
+        },
+        {
+            "name": "Demetrius",
+            "date": "19",
+            "season": "summer"
+        },
+        {
+            "name": "Dwarf",
+            "date": "22",
+            "season": "summer"
+        },
+        {
+            "name": "Willy",
+            "date": "24",
+            "season": "summer"
+        },
+        {
+            "name": "Penny",
+            "date": "2",
+            "season": "fall"
+        },
+        {
+            "name": "Elliott",
+            "date": "5",
+            "season": "fall"
+        },
+        {
+            "name": "Jodi",
+            "date": "11",
+            "season": "fall"
+        },
+        {
+            "name": "Abigail",
+            "date": "13",
+            "season": "fall"
+        },
+        {
+            "name": "Sandy",
+            "date": "15",
+            "season": "fall"
+        },
+        {
+            "name": "Marnie",
+            "date": "18",
+            "season": "fall"
+        },
+        {
+            "name": "Robin",
+            "date": "21",
+            "season": "fall"
+        },
+        {
+            "name": "George",
+            "date": "24",
+            "season": "fall"
+        },
+        {
+            "name": "Krobus ",
+            "date": "1",
+            "season": "winter"
+        },
+        {
+            "name": "Linus",
+            "date": "3",
+            "season": "winter"
+        },
+        {
+            "name": "Caroline",
+            "date": "7",
+            "season": "winter"
+        },
+        {
+            "name": "Sebastian",
+            "date": "10",
+            "season": "winter"
+        },
+        {
+            "name": "Harvey",
+            "date": "14",
+            "season": "winter"
+        },
+        {
+            "name": "Wizard",
+            "date": "17",
+            "season": "winter"
+        },
+        {
+            "name": "Evelyn",
+            "date": "20",
+            "season": "winter"
+        },
+        {
+            "name": "Leah",
+            "date": "23",
+            "season": "winter"
+        },
+        {
+            "name": "Clint",
+            "date": "26",
+            "season": "winter"
+        }
+    ],
+    "events": [
+        {
+            "name": "Egg Festival",
+            "date": "13",
+            "season": "spring"
+        },
+        {
+            "name": "Flower Dance",
+            "date": "24",
+            "season": "spring"
+        },
+        {
+            "name": "SalmonBerry Season (15-18)",
+            "date": "18",
+            "season": "spring"
+        },
+        {
+            "name": "Luau",
+            "date": "11",
+            "season": "summer"
+        },
+        {
+            "name": "Dance of the Moonlight Jellies",
+            "date": "28",
+            "season": "summer"
+        },
+        {
+            "name": "Extra Beach Foragables (12-14)",
+            "date": "14",
+            "season": "summer"
+        },
+        {
+            "name": "Stardew Valley Fair",
+            "date": "16",
+            "season": "fall"
+        },
+        {
+            "name": "Spirit's Eve",
+            "date": "27",
+            "season": "fall"
+        },
+        {
+            "name": "Blackberries (8-11)",
+            "date": "11",
+            "season": "fall"
+        },
+        {
+            "name": "Festival of Ice",
+            "date": "8",
+            "season": "winter"
+        },
+        {
+            "name": "Night Market(15-17",
+            "date": "17",
+            "season": "winter"
+        },
+        {
+            "name": "Feast of the Winter Star",
+            "date": "25",
+            "season": "winter"
+        },
+    ]
+
+}
--- stardew-helper.js
+++ stardew-helper.js
@@ -1,177 +1,4 @@
-var data = {
-    "birthdays": [
-        {
-            "person": "Kent",
-            "date": "4",
-            "season": "spring"
-        },
-        {
-            "person": "Lewis",
-            "date": "7",
-            "season": "spring"
-        },
-        {
-            "person": "Vincent",
-            "date": "3",
-            "season": "spring"
-        },
-        {
-            "person": "Haley",
-            "date": "14",
-            "season": "spring"
-        },
-        {
-            "person": "Pam",
-            "date": "18",
-            "season": "spring"
-        },
-        {
-            "person": "Shane",
-            "date": "20",
-            "season": "spring"
-        },
-        {
-            "person": "Pierre",
-            "date": "26",
-            "season": "spring"
-        },
-        {
-            "person": "Emily",
-            "date": "27",
-            "season": "spring"
-        },
-        {
-            "person": "Jas",
-            "date": "04",
-            "season": "summer"
-        },
-        {
-            "person": "Gus",
-            "date": "08",
-            "season": "summer"
-        },
-        {
-            "person": "Maru",
-            "date": "10",
-            "season": "summer"
-        },
-        {
-            "person": "Alex",
-            "date": "13",
-            "season": "summer"
-        },
-        {
-            "person": "Sam",
-            "date": "17",
-            "season": "summer"
-        },
-        {
-            "person": "Demetrius",
-            "date": "19",
-            "season": "summer"
-        },
-        {
-            "person": "Dwarf",
-            "date": "22",
-            "season": "summer"
-        },
-        {
-            "person": "Willy",
-            "date": "24",
-            "season": "summer"
-        },
-        {
-            "person": "Penny",
-            "date": "2",
-            "season": "fall"
-        },
-        {
-            "person": "Elliott",
-            "date": "5",
-            "season": "fall"
-        },
-        {
-            "person": "Jodi",
-            "date": "11",
-            "season": "fall"
-        },
-        {
-            "person": "Abigail",
-            "date": "13",
-            "season": "fall"
-        },
-        {
-            "person": "Sandy",
-            "date": "15",
-            "season": "fall"
-        },
-        {
-            "person": "Marnie",
-            "date": "18",
-            "season": "fall"
-        },
-        {
-            "person": "Robin",
-            "date": "21",
-            "season": "fall"
-        },
-        {
-            "person": "George",
-            "date": "24",
-            "season": "fall"
-        },
-        {
-            "person": "Krobus ",
-            "date": "1",
-            "season": "winter"
-        },
-        {
-            "person": "Linus",
-            "date": "3",
-            "season": "winter"
-        },
-        {
-            "person": "Caroline",
-            "date": "7",
-            "season": "winter"
-        },
-        {
-            "person": "Sebastian",
-            "date": "10",
-            "season": "winter"
-        },
-        {
-            "person": "Harvey",
-            "date": "14",
-            "season": "winter"
-        },
-        {
-            "person": "Wizard",
-            "date": "17",
-            "season": "winter"
-        },
-        {
-            "person": "Evelyn",
-            "date": "20",
-            "season": "winter"
-        },
-        {
-            "person": "Leah",
-            "date": "23",
-            "season": "winter"
-        },
-        {
-            "person": "Clint",
-            "date": "26",
-            "season": "winter"
-        },
-        
-    ]
-}
-
 var CurrentDate;
-var BirthDays;
-
 
 function StarDate() {
     var self = this;
@@ -201,7 +28,6 @@ function StarDate() {
 
     self.getSeason = function () {
         var season = Math.ceil(self.value / 28 % 4) || 1;
-        console.log(season);
         switch (season) {
             case 1:
             case "spring":
@@ -234,7 +60,7 @@ function StarDate() {
 function Load() {
     // get data from localstorage
     BindButtons();
-    CurrentDate = new StarDate(100);
+    CurrentDate = new StarDate(1);
 
     var datespot = document.querySelector('[data-bind=currentDate]');
     datespot.innerHTML = CurrentDate;
@@ -243,12 +69,17 @@ function Load() {
 }
 
 function LoadBirthdays() {
-    if (BirthDays == null) {
-        BirthDays = data.birthdays.map(function (data) {
-            return { Name: data.person, Date: new StarDate(data.season, data.date) }
-        })
-    }
-    upcomingBirthdays = BirthDays.filter(function (birthday) {
+    var events = data.events.concat(data.birthdays);
+
+    allEvents = events.map(function (data) {
+        return { Name: data.name, Date: new StarDate(data.season, data.date) }
+    })
+
+    allEvents.sort(function (a, b) {
+        return a.Date.value - b.Date.value;
+    })
+
+    upcomingEvents = allEvents.filter(function (birthday) {
         var diff = birthday.Date.value - CurrentDate.value;
         return diff < 16 && diff >= 0;
     })
@@ -256,12 +87,11 @@ function LoadBirthdays() {
 
     var birthdayText = document.querySelector("[data-foreach=birthdays]");
     birthdayText.innerHTML = "";
-    for (i of upcomingBirthdays) {
+    for (i of upcomingEvents) {
         var elem = document.createElement('li');
         elem.innerHTML = i.Name + " " + i.Date;
         birthdayText.appendChild(elem);
     }
-
 }
 
 function BindButtons() {
@@ -269,7 +99,6 @@ function BindButtons() {
 }
 
 function NextDay() {
-    console.log("advancing day")
     CurrentDate.AddDay(1);
     var datespot = document.querySelector('[data-bind=currentDate]');
     datespot.innerHTML = CurrentDate;
